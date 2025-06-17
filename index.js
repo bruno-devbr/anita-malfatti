b5faa17276297e25a129a036e95e19c060e0b1e8
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-06-16 23:04:45 -0300

    carrossel de imgs

--- index.js
+++ index.js
@@ -13,3 +13,91 @@ setInterval(() => {
     }
     estado = !estado;
 }, 10000);
+
+let i = 0;
+
+const imagesDiv = document.querySelector(".images");
+const stopAnimation_btn = document.querySelector("#stopAnimation_input");
+const inputIndex = document.querySelectorAll(".imagesIndex input");
+
+imagesDiv.scrollBy(-3150, 0);
+
+let intervalo;
+stopAnimation_btn.checked = false;
+
+inputIndex.forEach((input, index) => {
+    input.checked = index === 0;
+});
+
+function animationFunct() {
+    if (stopAnimation_btn.checked == false) {
+        intervalo = setInterval(() => {
+            imagesDiv.scrollBy(450, 0);
+            i++;
+
+            if (i >= 8) {
+                i = 0;
+                imagesDiv.scrollBy(-3600, 0);
+            }
+
+            if (i == 0) {
+                inputIndex[7].checked = false;
+            } else {
+                inputIndex[i - 1].checked = false;
+            }
+
+            inputIndex[i].checked = true;
+        }, 1000);
+    } else {
+        clearInterval(intervalo);
+    }
+}
+
+stopAnimation_btn.addEventListener("click", animationFunct);
+animationFunct();
+
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+
+function passArtLeft() {
+    if (imagesDiv.scrollLeft === 0) {
+        imagesDiv.scrollBy(3150, 0);
+        i = 7;
+    } else {
+        imagesDiv.scrollBy(-450, 0);
+        i--;
+    }
+
+    if (i == 7) {
+        inputIndex[0].checked = false;
+    } else {
+        inputIndex[i + 1].checked = false;
+    }
+
+    inputIndex[i].checked = true;
+}
+
+function passArtRight() {
+    if (imagesDiv.scrollLeft === 3150) {
+        imagesDiv.scrollBy(-3150, 0);
+        i = 0;
+    } else {
+        imagesDiv.scrollBy(450, 0);
+        i++;
+    }
+
+    inputIndex[i].checked = true;
+
+    if (i == 8) {
+        i = 0;
+    }
+
+    if (i == 0) {
+        inputIndex[7].checked = false;
+    } else {
+        inputIndex[i - 1].checked = false;
+    }
+}
+
+leftArrow.addEventListener("click", passArtLeft);
+rightArrow.addEventListener("click", passArtRight);
